f8d87ec3a19936c56c0b5c9a0a5b44d01d315e5c
xufengli <xufengli@uk.ibm.com>
2017-03-13 18:32:45 +0000

    add build script

---
+++ .travis/build.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+#-- script to automate preinstall, server compile, and package
+echo "preinstall server dependencies..."
+cd ./server
+npm install
+echo "compile server component..."
+npm run compile
+
+echo "preinstall client dependencies..."
+cd ../client
+npm install
+echo "package the VSIX file..."
+npm run package
+echo "build successfully!"
--- .travis/install.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-#-- script to automate 
