1cd7222a50c238414697270236667edf7319639e
xufengli <xufengli@uk.ibm.com>
2017-03-16 18:06:20 +0000

    update deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,8 +4,10 @@
 set -ev
 set -o pipefail
 
+cd ./client
+
 # check that this is the right node.js version
-if [ "${TRAVIS_NODE_VERSION}" != "" -a "${TRAVIS_NODE_VERSION}" != "6"]; then
+if [ "${TRAVIS_NODE_VERSION}" != "" -a "${TRAVIS_NODE_VERSION}" != "6" ]; then
   echo Not executing as not running primary node.js version
   exit 0
 fi
@@ -13,7 +15,7 @@ echo Start to install vsce
 echo "${VSCETOKEN}"
 echo "${TRAVIS_TAG}"
 # Push the code to npm there there is a travis tag defined
-if [ "${TRAVIS_TAG}" != "" ]; then 
+if [ "${TRAVIS_TAG}" != "" ]; then
   # Check  vsce installed
   npm install -g vsce
 
