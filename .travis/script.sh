a8cf49e84a2cf994a74891edaacef01ce76225af
m-g-k <mgk@uk.ibm.com>
2017-12-15 16:09:02 +0000

    Fix build break
    Signed-off-by: m-g-k <mgk@uk.ibm.com>

--- .travis/script.sh
+++ .travis/script.sh
@@ -5,11 +5,11 @@ set -ev
 set -o pipefail
 
 cd ./server
-npm run compile
+npm run compile:server
 
 cd ../client
 
-npm run package
+npm run package:vsix
 npm install -g vsce
 
 npm test 2>&1 | tee
